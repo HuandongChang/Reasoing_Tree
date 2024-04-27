31fd03723de0d01338643bb95bc8159a119ae379
Mingyuan Ma <mingyuan_ma@g.harvard.edu>
2024-04-27 01:14:47 -0400

    finished v1

--- models/TogetherAI_API.py
+++ models/TogetherAI_API.py
@@ -26,20 +26,6 @@ keys = [
     "7b1925f49e33f63f6d64c26a6bbf78ec4373e8d5df205ac785573311a4acd491",
     "c9e585d8fed30b7275d4b1244d1e0810cfd2593151ed16ac58a1753b49709c23",
     "621c928c7d8722869036cdda8b87077363dc2874aadda409419b66abb43a5148",
-    '1d328918b6c9af31dfafc1c6751890e52481c75866448c9db364a7e58c666814',
-    '7b1925f49e33f63f6d64c26a6bbf78ec4373e8d5df205ac785573311a4acd491',
-    '3668e80e292c32d8a23ed01bddc8d4efabc786ede0ea7d40d2de3c8ca49d6015',
-    'd16049df3f5732d9bf180f9b0d79947d6c20471bc9d9277166c5b6e180cef5cf',
-    "fb9262df6fe23a25f35355b42cc4d3182754fd9d186309aad0cd4f52fe9c8306",
-    '31a5be9a66b0bf3e10d877640b7ae5f73464949c157d7d01cdb6de1f16f53fbb',
-    'df2fbd8b27413e6a3b5d87a8df44c0ccfc7a175d19445a5f92a9a29450c1ec53',
-    'd202990ef50e94b0df994bc9d35f75cfe90f91f4aa126b08e33dd391d3e4a4e6',
-    "c9ea3ceced7d13f297a2388e48565ade965e451df25910e31e8a32919603ae92",
-    "045d2cf90a4ebca1997ee66acd5ea0d0fc83c99b281429ca21af42e486fe4be0",
-    "d3a3fd41900ad88e52855d0100e128a089eec5fa4e65818453ec9a41077fcf01",
-    "380775a5d7ac61cce84c5aefad7f0b28ec83bebb94632ad01e227ec1f0a3c34e",
-    "1ef5fdc7f7e072f3280886dcb9b7883187cee93a87dd40a37cf6602ee39850d4",
-    "acc82a8f44d765ce1462a2e047e5e6d1c434a4f5974d12e1aa526320cf552969",
 ]       # Add your togetherAI API keys here
 
 
@@ -74,9 +60,10 @@ def call_TogetherAI(prompt, model_ckpt, max_tokens=256, temperature=0.8, top_k=4
     while output is None:
         try:
             output = call(prompt, model_ckpt, max_tokens, temperature, top_k, top_p, stop=["\n", "\n\n"])
-        except:
+        except Exception as e:
+            print(e)
             print("TogetherAI call failed. Sleeping...")
-            time.sleep(5)
+            time.sleep(1)
 
     return output
 
